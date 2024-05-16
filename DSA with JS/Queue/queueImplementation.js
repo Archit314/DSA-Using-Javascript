60ab68e207390e09548b92a56d2a59a661d72cc5
Archit Kumar <architkumar314@gmail.com>
2024-05-16 09:26:37 +0530

    Added check to check wheather queue is full or not during enqueue

--- DSA with JS/Queue/queueImplementation.js
+++ DSA with JS/Queue/queueImplementation.js
@@ -9,9 +9,14 @@ class Queue {
     this.front = -1;
   }
   enqueue(element) {
+    if (this.rare >= this.items.length) {
+      const message = `Queue is full you cannot enqueue in queue`;
+      return { messaage: message, status: 422 };
+    }
     this.rare += 1;
     if (this.rare == 0) this.front += 1;
     this.items[this.rare] = element;
+    return { message: `Enqueued ${element} in queue`, status: 200 };
   }
   dequeue(element) {}
   firstElement() {
@@ -26,7 +31,11 @@ const queueArraySize = Number(prompt(`Enter the size of a queue: `));
 let queueArray = new Queue(queueArraySize);
 
 const element = Number(prompt(`Enter element to enqueue in queue: `));
-let updateArray = queueArray.enqueue(element);
+const insertElement = queueArray.enqueue(element);
+if (insertElement && insertElement.status == 422) {
+  return insertElement.messaage;
+}
+console.log(`Elements in queue: ${queueArray.items}`);
 
 const firstElement = queueArray.firstElement();
 console.log(`First element of queue is: ${firstElement}`);
